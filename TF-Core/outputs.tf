4cad370ff8c39d1c4ab1c78a6f811269ee5bd894
Bernardo <bteisceira@gmail.com>
2023-10-28 14:13:11 -0300

    ADD: TF-Core directory

---
+++ TF-Core/outputs.tf
@@ -0,0 +1,15 @@
+output "ecr_repo_url" {
+  value = module.ecr.ecr_repo_url
+}
+
+output "subnet_ids" {
+  value = module.network-services.subnet_id
+}
+
+output "sg_ids" {
+  value = module.network-services.sg_id
+}
+
+output "alb_tg_arns" {
+  value = module.network-services.alb_tg_arn
+}
